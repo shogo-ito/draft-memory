fe5d2ac1ea62e94d7b5d4a39b49cf734e164ddaa
EC2 Default User <ec2-user@ip-172-31-92-64.ec2.internal>
2019-08-31 13:24:12 +0000

    top page

---
+++ resources/views/card/card.blade.php
@@ -0,0 +1,69 @@
+<div class="m-4">
+    <div class="row text-white" style="background: green">
+        <h3 class="text-left font-weight-bold p-2">鈴木一朗（中京高校）</h3>
+    </div>
+    <div class="row" style="background: lightgray">
+        <h4 class="text-left font-weight-bold p-2">3年180cm78kg 右左 外野手</h4>
+    </div>
+    <div class="row">
+        <div class="col-sm-4" style="background: skyblue">
+            <h4 class="font-weight-bold text-center mt-3">選手評価</h6>
+                <ul class="list-unstyled text-center font-weight-bold mt-3 ">
+                    <li class="m-2">パワー：<span class="text-danger">A</span></li>
+                    <li class="m-2">ミート：<span class="text-success">B</span></li>
+                    <li class="m-2">走力　：<span class="text-warning">C</span></li>
+                    <li class="m-2">守備　：<span class="text-muted">D</span></li>
+                    <li class="m-2">将来性：<span class="text-dark">E</span></li>
+                </ul>
+        </div>
+        <div class="col-sm-8" style="background: ivory">
+            <h6 class="font-weight-bold mt-3">参考動画URL：</h6>
+                <p class="font-weight-light">xxxxxxxxxx/youtube.com</p>
+            <h6 class="font-weight-bold mt-3">参考記事URL：</h6>
+                <p class="font-weight-light">xxxxxxxxxx/xxxxxxx.com</p>
+            <h6 class="font-weight-bold mt-3">コメント：</h6>
+                <p class="font-weight-light">
+                    良い選手です。良い選手です。良い選手です。良い選手です。良い選手です。良い選手です。
+                    良い選手です。良い選手です。良い選手です。良い選手です。良い選手です。良い選手です。
+                    良い選手です。良い選手です。良い選手です。良い選手です。良い選手です。良い選手です。
+                    良い選手です。良い選手です。良い選手です。良い選手です。良い選手です。良い選手です。
+                </p>
+        </div>
+    </div>
+</div>
+
+
+<div class="m-4">
+    <div class="row text-white" style="background: green">
+        <h3 class="text-left font-weight-bold p-2">鈴木一朗（中京高校）</h3>
+    </div>
+    <div class="row" style="background: lightgray">
+        <h4 class="text-left font-weight-bold p-2">3年180cm78kg 右左 外野手</h4>
+    </div>
+    <div class="row">
+        <div class="col-sm-4" style="background: skyblue">
+            <h4 class="font-weight-bold text-center mt-3">選手評価</h6>
+                <ul class="list-unstyled text-center font-weight-bold mt-3 ">
+                    <li class="m-2">パワー：<span class="text-danger">A</span></li>
+                    <li class="m-2">ミート：<span class="text-success">B</span></li>
+                    <li class="m-2">走力　：<span class="text-warning">C</span></li>
+                    <li class="m-2">守備　：<span class="text-muted">D</span></li>
+                    <li class="m-2">将来性：<span class="text-dark">E</span></li>
+                </ul>
+        </div>
+        <div class="col-sm-8" style="background: ivory">
+            <h6 class="font-weight-bold mt-3">参考動画URL：</h6>
+                <p class="font-weight-light">xxxxxxxxxx/youtube.com</p>
+            <h6 class="font-weight-bold mt-3">参考記事URL：</h6>
+                <p class="font-weight-light">xxxxxxxxxx/xxxxxxx.com</p>
+            <h6 class="font-weight-bold mt-3">コメント：</h6>
+                <p class="font-weight-light">
+                    良い選手です。良い選手です。良い選手です。良い選手です。良い選手です。良い選手です。
+                    良い選手です。良い選手です。良い選手です。良い選手です。良い選手です。良い選手です。
+                    良い選手です。良い選手です。良い選手です。良い選手です。良い選手です。良い選手です。
+                    良い選手です。良い選手です。良い選手です。良い選手です。良い選手です。良い選手です。
+                </p>
+        </div>
+    </div>
+</div>
+
